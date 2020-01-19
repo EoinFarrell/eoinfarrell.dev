ce8fd27937745e22a32e7c45bbe252421b86afaf
EoinFarrell <efarrell@zendesk.com>
2020-01-19 16:12:08 +0000

    Adding support for photo gallary

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,15 @@ module.exports = {
   siteMetadata: {
     title: `eoinfarrell.dev`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    keywords: ["tech", "blog", "boop"],
     author: `@eoinfarrell`,
+    siteUrl: 'https://eoinfarrell.dev/',
+    siteImage: 'name-of-open-graphy-image.jpg', // pop an image in the static folder to use it as og:image
+    config: {
+      twitter: 'eoinfarrell', // no need to include the @
+      github: 'eoinfarrell239',
+      linkedin: 'eoinfarrell'
+    },
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -45,5 +53,19 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve:`gatsby-source-cloudinary`,
+      options:{
+        cloudName: 'eoinfarrell',
+        apiKey: '482726426563678',
+        apiSecret: 'SO6Ujg7pVh2eIi0kKb00DUo-OII',
+        // resourceType: `image`,
+        // type: `type Value`,
+        maxResults: `500`,
+        tags:`2019`
+        // prefix: ``
+      },
+    },
+    'react-image-gallery'
   ],
 }
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,14 +2,32 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 
+import ImageGallery from 'react-image-gallery'
+import "react-image-gallery/styles/css/image-gallery.css";
+
 export default ({ data }) => {
   const post = data.markdownRemark
+  const images = data.allCloudinaryMedia
+
+  var galleryImages = [];
+
+  for (var i = 0; i < images.edges.length; i++) {
+    galleryImages.push({
+      original: images.edges[i].node.url
+    })
+  }
+  
+
   return (
     <Layout>
       <div>
+        {}
+        {/* <h1>{images.edges.node.url}</h1> */}
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
+
+      <ImageGallery items={galleryImages} />
     </Layout>
   )
 }
@@ -20,6 +38,13 @@ export const query = graphql`
       html
       frontmatter {
         title
+      }
+    }
+    allCloudinaryMedia {
+      edges {
+        node {
+          url
+        }
       }
     }
   }
